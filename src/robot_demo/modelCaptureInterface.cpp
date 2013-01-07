3ebdcbfbf24daa6ea15bc39213280c0fde0eb7b7
Vincent Rabaud <vrabaud@willowgarage.com>
2013-01-07 22:18:46 +0100

    get the robot demo compiling against the new model capture API

--- src/robot_demo/modelCaptureInterface.cpp
+++ src/robot_demo/modelCaptureInterface.cpp
@@ -1,12 +1,79 @@
 #include "modelCaptureInterface.hpp"
 #include "rosUtils.hpp"
 #include <opencv2/opencv.hpp>
+#include <opencv2/rgbd/rgbd.hpp>
+
+#include "../../../opencv_candidate/src/rgbd/samples/reconst3d/graph_optimizations.hpp"
 
 using namespace cv;
 using std::cout;
 using std::endl;
 
 //#define SAVE_COLLECTED_DATA
+
+static void
+preparePosesLinksWithoutRt(const vector<PosesLink>& srcLinks, vector<PosesLink>& dstLinks)
+{
+    dstLinks.resize(srcLinks.size());
+    for(size_t i = 0; i < dstLinks.size(); i++)
+        dstLinks[i] = PosesLink(srcLinks[i].srcIndex, srcLinks[i].dstIndex);
+}
+
+static Mat
+refineObjectMask(const Mat& initObjectMask)
+{
+    vector<vector<Point> > contours;
+    Mat initObjectMaskClone = initObjectMask.clone();
+    findContours(initObjectMaskClone, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    if(contours.empty())
+        return initObjectMask.clone();
+
+    int maxMaskArea = 0.;
+    int objectMaskIndex = -1;
+    for(size_t i = 0; i < contours.size(); i++)
+    {
+        Mat mask(initObjectMask.size(), CV_8UC1, Scalar(0));
+        drawContours(mask, contours, i, Scalar(255), CV_FILLED, 8);
+
+        int curMaskArea = countNonZero(mask);
+        if(curMaskArea > maxMaskArea)
+        {
+            maxMaskArea = curMaskArea;
+            objectMaskIndex = i;
+        }
+    }
+
+    Mat objectMask(initObjectMask.size(), CV_8UC1, Scalar(0));
+    drawContours(objectMask, contours, objectMaskIndex, Scalar(255), CV_FILLED, 8);
+
+    return objectMask & initObjectMask;
+}
+
+static void
+prepareFramesForModelRefinement(const Ptr<TrajectoryFrames>& trajectoryFrames, const vector<int>& frameIndices, vector<Ptr<RgbdFrame> >& dstFrames)
+{
+    dstFrames.resize(trajectoryFrames->frames.size());
+    for(size_t frameIndex = 0; frameIndex < dstFrames.size(); frameIndex++)
+    {
+        const Ptr<RgbdFrame> srcFrame = trajectoryFrames->frames[frameIndex];
+        if(std::find(frameIndices.begin(), frameIndices.end(), frameIndex) != frameIndices.end())
+        {
+            // clone data for used frames because we can modify them
+            dstFrames[frameIndex] = new RgbdFrame(srcFrame->image.clone(), srcFrame->depth.clone(),
+                                         refineObjectMask(trajectoryFrames->objectMasks[frameIndex]), srcFrame->normals.clone(),
+                                         srcFrame->ID);
+        }
+        else
+        {
+            dstFrames[frameIndex] = new RgbdFrame(srcFrame->image, srcFrame->depth,
+                                         refineObjectMask(trajectoryFrames->objectMasks[frameIndex]), srcFrame->normals,
+                                         srcFrame->ID);
+        }
+    }
+}
+
+
+
 
 ModelCapturer::ModelCapturer()
 {
@@ -37,15 +104,14 @@ void ModelCapturer::addRGBDFrame(const cv::Mat &bgrImage, const cv::Mat &depth)
 
   static int frameID = 0;
   cout << "pushing frame..." << std::flush;
-  bool isKeyframe;
-  Mat currentPose = onlineCaptureServer.push(bgrImage, depth, frameID, &isKeyframe);
+  Ptr<OnlineCaptureServer::FramePushOutput> odometry_frame = onlineCaptureServer.push(bgrImage, depth, frameID);
   cout << " done." << endl;
   ++frameID;
 
-  if (isKeyframe)
+  if (odometry_frame->frameState == TrajectoryFrames::KEYFRAME)
   {
     //TODO: move up
-    publishOdometry(currentPose, "RGBDOdometry");
+    publishOdometry(odometry_frame->pose, "RGBDOdometry");
   }
 
   allBgrImages.push_back(bgrImage);
@@ -53,7 +119,7 @@ void ModelCapturer::addRGBDFrame(const cv::Mat &bgrImage, const cv::Mat &depth)
   allDepths.push_back(depth);
 #endif
 
-  isLoopClosed = onlineCaptureServer.isLoopClosed();
+  isLoopClosed = onlineCaptureServer.isLoopClosedFunc();
   if (isLoopClosed)
   {
     createModel();
@@ -89,49 +155,46 @@ void ModelCapturer::createModel()
 
 
 
-  Ptr<KeyframesData> keyframesData = onlineCaptureServer.finalize();
+  cv::Ptr<TrajectoryFrames> keyframesData = onlineCaptureServer.finalize();
+
+
+  const float voxelSize = 0.005f;
 
   cout << "Frame-to-frame odometry result" << endl;
-  //TODO: move up
-//  publishOdometry(keyframesData->poses, "RGBDOdometry");
-  showModel(allBgrImages, keyframesData->frames, keyframesData->poses, cameraMatrix, 0.005);
+  ModelReconstructor::genModel(keyframesData->frames, keyframesData->poses, cameraMatrix)->show(voxelSize);
 
   vector<Mat> refinedPosesSE3;
-  refineSE3Poses(keyframesData->poses, refinedPosesSE3);
+  vector<int> frameIndices;
+  refineGraphSE3(keyframesData->poses, keyframesData->keyframePosesLinks, refinedPosesSE3, frameIndices);
 
   cout << "Result of the loop closure" << endl;
-  //TODO: move up
+  ModelReconstructor::genModel(keyframesData->frames, refinedPosesSE3, cameraMatrix, frameIndices)->show(voxelSize);
   publishOdometry(refinedPosesSE3, "LoopClosure");
-  showModel(allBgrImages, keyframesData->frames, refinedPosesSE3, cameraMatrix, 0.003);
 
-  vector<Mat> refinedPosesICPSE3;
-  float pointsPart = 0.05f;
-  refineRgbdICPSE3Poses(keyframesData->frames, refinedPosesSE3, cameraMatrix, pointsPart, refinedPosesICPSE3);
+  // fill posesLinks with empty Rt because we want that they will be recomputed
+  vector<PosesLink> keyframePosesLinks;
+  preparePosesLinksWithoutRt(keyframesData->keyframePosesLinks, keyframePosesLinks);
+
+  vector<Mat> refinedPosesSE3RgbdICP;
+  const float pointsPart = 0.05f;
+  refineGraphSE3RgbdICP(keyframesData->frames, refinedPosesSE3,
+                        keyframePosesLinks, cameraMatrix, pointsPart, refinedPosesSE3RgbdICP, frameIndices);
 
   cout << "Result of RgbdICP for camera poses" << endl;
-  //TODO: move up
-  publishOdometry(refinedPosesICPSE3, "RgbdICP");
-  float modelVoxelSize = 0.003f;
-  showModel(allBgrImages, keyframesData->frames, refinedPosesICPSE3, cameraMatrix, modelVoxelSize);
+  ModelReconstructor::genModel(keyframesData->frames, refinedPosesSE3RgbdICP, cameraMatrix, frameIndices)->show(/*voxelSize*/);
+  publishOdometry(refinedPosesSE3RgbdICP, "RgbdICP");
 
-#if 1
-  // remove table from the further refinement
-  for(size_t i = 0; i < keyframesData->frames.size(); i++)
-  {
-      keyframesData->frames[i]->mask &= ~keyframesData->tableMasks[i];
-      keyframesData->frames[i]->releasePyramids();
-  }
-  pointsPart = 1.f;
-  modelVoxelSize = 0.001;
-#endif
+  vector<Ptr<RgbdFrame> > objectFrames; // with mask for object points only,
+                                        // they will modified while refining the object points
+  prepareFramesForModelRefinement(keyframesData, frameIndices, objectFrames);
 
-  vector<Mat> refinedPosesICPSE3Landmarks;
-  refineICPSE3Landmarks(keyframesData->frames, refinedPosesICPSE3, cameraMatrix, refinedPosesICPSE3Landmarks);
+  vector<Mat> refinedSE3ICPSE3ModelPoses;
+  refineGraphSE3RgbdICPModel(objectFrames, refinedPosesSE3RgbdICP,
+                             keyframePosesLinks, cameraMatrix, refinedSE3ICPSE3ModelPoses, frameIndices);
 
-  cout << "Result of RgbdICP for camera poses and moving the model points" << endl;
-  //TODO: move up
-  publishOdometry(refinedPosesICPSE3Landmarks, "RgbdICP_landmarks");
-  modelVoxelSize = 0.000001;
-  showModel(allBgrImages, keyframesData->frames, refinedPosesICPSE3Landmarks, cameraMatrix, modelVoxelSize);
-//    showModelWithNormals(allBgrImages, keyframesData->frame, refinedPosesICPSE3Landmarks, cameraMatrix);
+  cv::Ptr<ObjectModel> model = ModelReconstructor::genModel(objectFrames, refinedSE3ICPSE3ModelPoses, cameraMatrix, frameIndices);
+
+  cout << "Result of RgbdICP  for camera poses and model points refinement" << endl;
+  model->show();
+  publishOdometry(refinedSE3ICPSE3ModelPoses, "RgbdICP_landmarks");
 }
