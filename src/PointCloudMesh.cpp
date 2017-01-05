412165bf1a770ca8b0d992ba8e41a0ba682dd953
v4hn <me@v4hn.de>
2017-01-05 18:14:42 +0100

    add missing boost/format include
    This is used below but was not included.
    This breaks my build over here.

--- src/PointCloudMesh.cpp
+++ src/PointCloudMesh.cpp
@@ -10,6 +10,8 @@
 #include <ecto_pcl/ecto_pcl.hpp>
 #include <sensor_msgs/PointField.h>
 
+#include <boost/format.hpp>
+
 using ecto::tendrils;
 using ecto::pcl::xyz_cloud_variant_t;
 using ecto::tendrils;
